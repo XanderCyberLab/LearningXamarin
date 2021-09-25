f4c99feff40ff2dbccea7088cc3c0178a8fc46e9
Alexander Morales <amoralesmm@yahoo.com>
2021-09-25 03:54:39 -0500

    Learn how to create a similar CSS Sheet. Creating Resources in the App xaml.

--- LearningXamarin/LearningXamarin/NewNote.xaml.cs
+++ LearningXamarin/LearningXamarin/NewNote.xaml.cs
@@ -10,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace LearningXamarin
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePage : TabbedPage
+    public partial class NewNote : ContentPage
     {
-        public HomePage()
+        public NewNote()
         {
             InitializeComponent();
-        }
-
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new NewNote()); // Code to go to NewNote Page
         }
     }
 }
